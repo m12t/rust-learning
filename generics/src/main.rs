198e7fdb252694890345aa5ecf4d76618f09da59
Michael Willett <51060085+m12t@users.noreply.github.com>
2025-02-16 10:49:41 -0500

    generic methods

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,9 +1,25 @@
 fn main() {
-    println!("Hello, world!");
-    let out = combine("a", "b");
-    println!("{}", out)
+    let pnt = Point { a: 5, b: 5, c: "c" };
+    let a = pnt.a();
+    let b = pnt.b();
+    let c = pnt.c();
+    println!("{a}::{b}::{c}")
 }
 
-fn combine<T>(a: T, b: T) -> T {
-    a + b
+struct Point<T1, T2> {
+    a: T1,
+    b: T1,
+    c: T2,
+}
+
+impl<T, U> Point<T, U> {
+    fn a(&self) -> &T {
+        &self.a
+    }
+    fn b(&self) -> &T {
+        &self.b
+    }
+    fn c(&self) -> &U {
+        &self.c
+    }
 }
