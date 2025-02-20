ebc040f37199843a1692aae338f9f9b2ff3e4956
Michael Willett <51060085+m12t@users.noreply.github.com>
2025-02-19 20:10:22 -0500

    lifetime tests

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    lifetimes();
+}
+
+fn lifetimes() {
+    let s1 = String::from("foo is longer");
+
+    {
+        let s2 = String::from("bar");
+        let longer = borrower(s1.as_str(), s2.as_str());
+
+        println!("{}", longer);
+    }
+}
+
+fn borrower<'a, 'b>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        return x;
+    }
+    return y;
 }
